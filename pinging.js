d874342cacdfe07c4fde5d5a4f46a0b4d755d8cf
Codegrapi <146641572+codegrapi@users.noreply.github.com>
2023-10-05 13:03:04 +0700

    Update pinging.js

--- pinging.js
+++ pinging.js
@@ -1,26 +1,32 @@
 <script>
-document.addEventListener("DOMContentLoaded", function() {
-    var expirationTimeInDays = 1;
-    var currentTimeInSeconds = Math.floor(new Date().getTime() / 1000);
+// Tanggal dan waktu target
+var targetDate = new Date("October 5, 2023 13:07:59").getTime();
 
-    var lastLicenseUpdateInSeconds = Math.floor(new Date().getTime() / 1000);
+// Update waktu mundur setiap 1 detik
+var countdown = setInterval(function() {
 
-    var expirationTimeInSeconds = expirationTimeInDays * 24 * 60 * 60;
-    var remainingTimeInSeconds = lastLicenseUpdateInSeconds + expirationTimeInSeconds - currentTimeInSeconds;
+  // Dapatkan tanggal dan waktu saat ini
+  var now = new Date().getTime();
 
-    if (remainingTimeInSeconds <= 0) {
-        window.location.href = "https://stagsite.online/tes/upgrade/";
-    }
-});
+  // Hitung selisih antara waktu target dan waktu saat ini
+  var distance = targetDate - now;
 
-</script>
+  // Hitung waktu mundur dalam hari, jam, menit, dan detik
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-<script>
-document.addEventListener("contextmenu", function(e) {
-    e.preventDefault();
-});
+  // Tampilkan waktu mundur dalam format "hari:jam:menit:detik"
+  console.log(days + " hari, " + hours + " jam, " + minutes + " menit, " + seconds + " detik");
 
-document.addEventListener("selectstart", function(e) {
-    e.preventDefault();
-});
+  // Hentikan perhitungan waktu mundur jika sudah mencapai target
+  if (distance < 0) {
+    clearInterval(countdown);
+    console.log("Waktu telah habis!");
+
+    // Redirect ke URL yang dituju setelah waktu habis
+    window.location.href = 'https://stagsite.online/tes/member-area/';
+      }
+    }, 1000);
 </script>
